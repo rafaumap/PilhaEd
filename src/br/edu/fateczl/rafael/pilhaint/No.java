3403458a70df8b781b3e97cb2b050257c6829da6
Rafa <Rafa@DESKTOP-QNIR2T8.home>
2021-09-16 00:05:20 -0300

    Primeiro Projeto

---
+++ src/br/edu/fateczl/rafael/pilhaint/No.java
@@ -0,0 +1,8 @@
+package br.edu.fateczl.rafael.pilhaint;
+
+public class No {
+	
+	int dado;
+	No proximo;
+	
+}
---
+++ src/br/edu/fateczl/rafael/pilhastring/No.java
@@ -0,0 +1,8 @@
+package br.edu.fateczl.rafael.pilhastring;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+}
